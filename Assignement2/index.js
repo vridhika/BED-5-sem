9839252455d9c25da86b04b0440502f5dae588e4
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2025-07-18 15:40:12 +0530

    lex 8

---
+++ Assignement2/index.js
@@ -0,0 +1,55 @@
+const { read } = require("./IO/io");
+const { write } = require("./IO/io");
+
+const username = process.argv[2];
+const productname = process.argv[3];
+
+async function buyProduct(username, productname) {
+  const usersData = await read("user.txt");
+  const productsData = await read("product.txt");
+
+  const users = JSON.parse(usersData);
+  const products = JSON.parse(productsData);
+
+  let user = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name === username) {
+      user = users[i];
+      break;
+    }
+  }
+
+  let product = null;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].product === productname) {
+      product = products[i];
+      break;
+    }
+  }
+
+  if (user == null) {
+    console.log("User nhi mila...");
+    return;
+  }
+
+  if (product == null) {
+    console.log("product nhi mila..");
+    return;
+  }
+  
+  o
+  
+
+  const newOrder = {
+    userName: user.name,
+    productName: product.product
+  };
+
+  orderHistory.push(newOrder);
+
+  await write("orderHistory.txt", JSON.stringify(orderHistory));
+
+  console.log("order done ..");
+}
+
+buyProduct(username, productname);
